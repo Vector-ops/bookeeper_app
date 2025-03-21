8ef8270c8fa9124c7399f970c94f5149cd355170
Vector-ops <sumith2827@gmail.com>
2025-03-21 12:23:14 +0530

    refactor: setup app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,75 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+				headerShown: true,
+				headerTitleStyle: {
+					fontSize: 30,
+					fontWeight: "heavy",
+				},
+				tabBarButton: HapticTab,
+				tabBarBackground: TabBarBackground,
+				tabBarStyle: Platform.select({
+					ios: {
+						position: "absolute",
+						backgroundColor: "black",
+					},
+					default: {
+						backgroundColor: "black",
+					},
+				}),
+			}}
+		>
+			<Tabs.Screen
+				name="home"
+				options={{
+					title: "BK",
+					tabBarIcon: ({ color }) => (
+						<FontAwesome5 name="home" size={24} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="add-goal"
+				options={{
+					title: "Add Goal",
+					tabBarIcon: ({ color }) => (
+						<AntDesign name="plus" size={24} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="progress"
+				options={{
+					title: "Progress",
+					tabBarIcon: ({ color }) => (
+						<AntDesign name="linechart" size={24} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="settings"
+				options={{
+					title: "Settings",
+					tabBarIcon: ({ color }) => (
+						<FontAwesome5 name="cog" size={24} color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
 }
